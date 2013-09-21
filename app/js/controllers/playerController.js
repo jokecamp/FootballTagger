4f086d49cf0d251cc6b6d9867b181f19abce5a5a
Joe Kampschmidt <jokecamp@gmail.com>
2013-09-21 12:44:37 -0400

    Player Controller Updates

--- app/js/controllers/playerController.js
+++ app/js/controllers/playerController.js
@@ -7,22 +7,39 @@ myApp.controller('playerController', ['$scope', '$http',
         $scope.players = data;
       });
 
-      $scope.addPlayer = function() {
-
-        $http.post('http://192.241.243.215/api/players/0', {
-          Firstname: $scope.Firstname,
-          Lastname: $scope.lastnameText,
-          Displayname: $scope.Firstname + " " + $scope.lastnameText
-        }).success(function(data) {
-
-          $http.get('http://192.241.243.215/api/players.json').success(function(data) {
-            $scope.players = data;
-          });
-        });
-
-        $scope.firstnameText = '';
-        $scope.lastnameText = '';
+      $scope.newPlayer = function() {
+        $scope.saveMode = "Add New";
+        $scope.player = { "Id":0 };
       };
 
+      $scope.loadPlayer = function (data) {
+        $scope.saveMode = "Save";
+        //console.log("load player " + data.Id);
+        $scope.player = data;
+      };
+
+      $scope.savePlayer = function() {
+
+        console.log($scope.player);
+        $scope.player.Displayname = $scope.player.Firstname + " " + $scope.player.Lastname;
+
+        if ($scope.player.Id > 0) {
+          console.log("need to implement save");
+        }
+        else {
+          $http.post('http://192.241.243.215/api/players/0', $scope.player).success(function(data) {
+
+            $http.get('http://192.241.243.215/api/players.json').success(function(data) {
+              $scope.players = data;
+            });
+          });
+        }
+
+        $scope.newPlayer();
+      };
+
+
+      // default settings
+      $scope.newPlayer();
     }
   ]);
